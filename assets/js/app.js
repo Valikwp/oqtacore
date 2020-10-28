7b6ffb1d76ab9afab680c67d63de7b2230f6111c
admin <admin@MacBook-Air-admin.local>
2020-10-28 17:41:43 +0200

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -11162,7 +11162,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: wrong number of arguments (4 for 3) for `rgb'\n        on line 100 of src/assets/styles/components/_header.scss\n        from line 2 of src/assets/styles/_sections.scss\n        from line 3 of /Users/admin/Downloads/oqtacore/src/assets/styles/style.scss\n>>     background-color: rgb(11, 9, 73, 0.3);\n\n   ----------------------^\n\n    at /Users/admin/Downloads/oqtacore/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/admin/Downloads/oqtacore/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/admin/Downloads/oqtacore/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/admin/Downloads/oqtacore/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/Users/admin/Downloads/oqtacore/node_modules/sass-loader/dist/index.js:73:7)\n    at Object.done [as callback] (/Users/admin/Downloads/oqtacore/node_modules/neo-async/async.js:8069:18)\n    at options.error (/Users/admin/Downloads/oqtacore/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
